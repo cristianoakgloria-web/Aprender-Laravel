662d31042adff4b70d45fb3c7a5523306ecde83f
Cristiano Glória <cristianoakgloria@gmail.com>
2026-02-05 11:58:45 +0100

    Aula 4

---
+++ laravel12/resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4">
+    <p class="text-center background-blue">
+        Criado por Crisnote. O código-fonte está disponível no <a href="" class="underline">GitHub</a>.
+    </p>
+</footer>
---
+++ laravel12/resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<header class="bg-white border-b-2 flex items-center justify-between p-4">
+    {{--Logo--}}
+    <div>
+        logo
+    </div>
+
+    {{-- GitHub --}}
+    <div>
+        github
+    </div>
+</header>
